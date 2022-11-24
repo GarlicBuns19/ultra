5971779fa6b32e3f311dbb8455525c78c646f1eb
GarlicBuns19 <planterx26@gmail.com>
2022-11-24 09:49:04 +0200

    Cors err

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
 // Env file
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 // The database
-const db = require("./config/dbMysql")
+const db = require("./config/dbMysql");
 // Express router
-const router = express.Router()
-const data = require("./routes/getData")
-const addedData = require("./routes/addData")
+const router = express.Router();
+const data = require("./routes/getData");
+const addedData = require("./routes/addData");
 // App and Port
-const port = parseInt(process.env.PORT) || 3000
-const app = express()
+const port = parseInt(process.env.PORT) || 3000;
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // Use Routes
-app.use(express.json())
-app.use('/ultra',data)
-app.use('/ultra',addedData)
+app.use(express.json());
+app.use("/ultra", data);
+app.use("/ultra", addedData);
 
-app.get("/",(req,res) => {
-    try{
-        res.sendFile(path.join(__dirname,'./public','./index.html'))
-    }catch(err){
-        res.json({
-            status : 400,
-            msg : "Home page not found",
-            error : err.message
-        })
-    }
-})
+app.get("/", (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, "./public", "./index.html"));
+  } catch (err) {
+    res.json({
+      status: 400,
+      msg: "Home page not found",
+      error: err.message,
+    });
+  }
+});
 
 // For all errors
-app.get('*',(req,res) => {
-    res.status(400).sendFile(path.join(__dirname,'./404','./404.html'))
-})
+app.get("*", (req, res) => {
+  res.status(400).sendFile(path.join(__dirname, "./404", "./404.html"));
+});
 
-app.listen(port,(err) => {
-    if(err){
-        res.json({
-            status : 400,
-            msg:"Cannot run sever"
-        })
-    }
-    console.log(`Sever is running on http://localhost:${port}`)
-})
-
+app.listen(port, (err) => {
+  if (err) {
+    res.json({
+      status: 400,
+      msg: "Cannot run sever",
+    });
+  }
+  console.log(`Sever is running on http://localhost:${port}`);
+});
